c0c23462b448bd51ddd16ba2a880922d8f9bdbc8
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-12-16 19:22:15 +0000

    feat: Set up initial structure for price predictions page.

--- app_pages/price_predictions_page.py
+++ app_pages/price_predictions_page.py
@@ -2,4 +2,27 @@ import streamlit as st
 
 def app():
     st.title("Sale Price Predictions")
-    st.write("This is the Price Predictions page. Contents to be added...")
+    st.markdown("### Predict house prices with ease!")
+
+    # Section for inherited houses predictions
+    st.header("Predicted Prices for Inherited Houses")
+    st.write("The predicted sale prices for the 4 inherited properties are displayed here.")
+    st.write("**Placeholder for predicted prices table**")
+
+    # Summed predicted price
+    st.subheader("Summed Sale Price")
+    st.write("The toal predicted price for all 4 inherited properties is displayed here.")
+    st.write("**Placeholder for total price message.**")
+
+    # Custom Predictions
+    st.header("Custom Price Predictions")
+    st.write(
+        """
+        Use the interactive form below to input house details and generate a predicted sale price:
+        """
+    )
+    st.write("**Placeholder for inherited widgets and predicted price display**")
+
+    # Visualization Placeholder
+    st.subheader("Predictions Visualization")
+    st.write("**Placeholder for predictions bar chart.**")
